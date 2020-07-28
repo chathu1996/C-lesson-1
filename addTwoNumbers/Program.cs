5218a6e2176ea6a9268bed525dc751888a89946b
iamchahu <chathurangacithrasena@gmail.com>
2020-07-29 00:29:46 +0530

    first commit

---
+++ addTwoNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace addTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            Console.WriteLine("Please Enter the First Number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter the Second Number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = num1 + num2;
+            Console.WriteLine("Sum is {0}", result);
+
+            Console.WriteLine("Please press any key to close window");
+            Console.ReadKey();
+        }
+    }
+}
